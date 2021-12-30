e45f9ff15073042e25e9a7cbb5db76cbdd6fd62c
James Wu <jameswu62@yahoo.co.nz>
2021-12-30 20:48:45 +1300

    refactor

--- src/printer-test.cpp
+++ src/printer-test.cpp
@@ -4,12 +4,12 @@
 
 namespace
 {
-    TEST(PrinterFoo, printIvalidInput_zero)
+    TEST(printerFoo, printIvalidInput_zero)
     {
         EXPECT_THROW(PrinterFoo printFoo(0), std::invalid_argument);
     }
 
-    TEST(PrinterFoo, invalidInput_negativeNumber)
+    TEST(printerFoo, invalidInput_negativeNumber)
     {
         EXPECT_THROW(PrinterFoo printFoo(-1), std::invalid_argument);
         EXPECT_THROW(PrinterFoo printFoo(-10), std::invalid_argument);
@@ -17,14 +17,14 @@ namespace
         EXPECT_THROW(PrinterFoo printFoo(INT_MIN), std::invalid_argument);
     }
 
-    TEST(PrinterFoo, invalidInput_greaterThan100)
+    TEST(printerFoo, invalidInput_greaterThan100)
     {
         EXPECT_THROW(PrinterFoo printFoo(101), std::invalid_argument);
         EXPECT_THROW(PrinterFoo printFoo(102), std::invalid_argument);
         EXPECT_THROW(PrinterFoo printFoo(INT_MAX), std::invalid_argument);
     }
 
-    TEST(PrinterFoo, validInputValues)
+    TEST(printerFoo, validInputValues)
     {
         const int RANGE_START = 1;
         const int RANGE_STOP = 33;
